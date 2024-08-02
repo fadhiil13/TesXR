7d0af3333c29af42d7850c654bb9677a166df905
fadhiil13 <fadhil_saputra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:57:36 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,10 @@
+public class Tes {
+ public static void main(String[] args) {
+    System.out.println();
+    int a = 1;
+    int b = 200;
+    int hasil = a+b;
+    System.out.println(" hasil dari " + a + " ditambah " + b + " addalah " + hasil);
+
+ }
+}
