df857fe483c4e725f5ab0aef225c6640c0054dc8
fadhiil13 <fadhil_saputra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:02:56 +0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,22 @@
+public class hello {
+   public static void main(String[] args) {
+    
+    String name= "Fadhil";
+    String asalKota="malang";
+    int usia=16;
+    double beratBadan=75.00;
+    float tinggiBadan=180.0f;
+    char jenisKelamin='l';
+    boolean status= false;
+    
+    System.out.println("Nama: "+name);
+    System.out.println("usia: "+usia);
+    System.out.println("asal Kota: "+asalKota);
+    System.out.println("tinggi Badan "+tinggiBadan);
+    System.out.println("jenis kelamin "+jenisKelamin);
+    System.out.println("status "+status);
+    
+
+
+   }
+ }
