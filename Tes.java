00182a23bd7c01d8e4ed91476490bed9ae22aead
fadhiil13 <fadhil_saputra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 10:54:13 +0700

    Add files via upload

--- Tes.java
+++ Tes.java
@@ -1,10 +1,21 @@
-public class Tes {
- public static void main(String[] args) {
-    System.out.println();
-    int a = 1;
-    int b = 200;
-    int hasil = a+b;
-    System.out.println(" hasil dari " + a + " ditambah " + b + " addalah " + hasil);
+import java.util.Scanner;
 
- }
+public class Tes {
+   public static void main(String[] args) {
+
+      System.out.println(" welcome to program pencari luas lingkaran");
+      double jari_jari;
+      double phi = 22/7;
+      double rumus;
+      Scanner Masukan = new Scanner (System.in);
+      
+      System.out.println("nilai jari jari = ");
+      jari_jari = Masukan.nextDouble();
+
+      rumus = phi*jari_jari*jari_jari;
+
+      System.out.println(" luas lingkaran = "+ rumus);
+
+
+   }
 }
