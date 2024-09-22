9dfe00e7c2d387687708f119f2f972f34feaf672
fadhiil13 <fadhil_saputra_33rpl@student.smktelkom-mlg.sch.id>
2024-09-22 17:44:05 +0700

    Tugas2.java

--- tugas bu aul/Tugas2.java
+++ tugas bu aul/Tugas2.java
@@ -4,10 +4,13 @@ public class Tugas2 {
     public static void main(String[] args) {
         int siswa, jumlahSiswa, nilaiSiswa = 0, rataRata, temp = 0;
         double totalNilai;
+        
         Scanner in = new Scanner(System.in);
+        
         System.out.println("Masukkan Jumlah Siswa: ");
         jumlahSiswa = in.nextInt();
         siswa = 1;
+        
         while (siswa <= jumlahSiswa) {
             System.out.print("Masukkan Nilai Siswa " + siswa + ":");
             nilaiSiswa = in.nextInt();
