56ead025d4f1988a290d57a3bebd04139663f853
fadhiil13 <fadhil_saputra_33rpl@student.smktelkom-mlg.sch.id>
2024-09-22 17:43:02 +0700

    Tugas1.java

--- tugas bu aul/Tugas1.java
+++ tugas bu aul/Tugas1.java
@@ -6,13 +6,14 @@ public class Tugas1 {
         int nilaiAwal, nilaiAkhir;
 
         Scanner in = new Scanner(System.in);
+        
         System.out.println("Masukkan Nilai Awal: ");
         nilaiAwal = in.nextInt();
         System.out.println("Masukkan Nilai Akhir: ");
         nilaiAkhir = in.nextInt();
+        
         i = nilaiAwal;
         while (i <= nilaiAkhir) {
-
             i++;
             if (i % 6 == 0) {
                 System.out.println(i);
